5a9d7097e0860971b66ef5390f5fd2030f290774
AliLynne <alilynne.thompson@gmail.com>
2020-01-09 16:42:54 -0800

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import headerStyles from './header.module.scss'
+
+const Header = () => {
+  return (
+    <div className={headerStyles.header}>
+      Header
+    </div>
+  )
+}
+
+export default Header
